d5562487bf35a9dafc688616b392f3f1635a3cdc
Sabrina Becker <sbecka8@gmail.com>
2019-12-09 17:47:20 -0500

    Learning about state with update time and date, and life-cycle of components

--- src/state/TheDate.js
+++ src/state/TheDate.js
@@ -1,15 +1,43 @@
 import React from 'react';
 
 class TheDate extends React.Component {
+    //the component is created
     constructor(props) {
         super(props)
         this.state = { datetime: new Date() };
+        console.log('constructor') //happens first once
     }
+    //Component has been added to the DOM
+    componentDidMount() {
+        console.log('componentDidMount') //happened third
+        this.interval = setInterval(() => {
+            console.log('setInterval') //happens 4th, then render
+            this.setState({
+                datetime: new Date() 
+            })
+        }, 1000)
+    }
+    //Component is about to be removed from the DOM
+    //if the parent component's render doesn't render this component anymore
+    //then this component will be removed
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+    //the component is reacting to being created or updated
+    //if component props or state change, the render can happen again
     render() {
+        console.log('render') //happened second
         return (
         <div>{this.state.datetime.toLocaleString()}</div>
         )
     }
+    //update the DOM=component is added or changed in the DOM
+    //If the component's render function returns somethin, it'll be mounted
+    //If the result of render is different from what's on the page it'll be updated
 };
+
+//setInterval and render happen continously per second
+//everytime setState is called a new render is triggered
+//unmount won't happen until we unmount the component
 
 export default TheDate
