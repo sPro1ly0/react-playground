83b52284b9028e771f47286ddaed4231e7d24eca
Sabrina Becker <sbecka8@gmail.com>
2019-12-09 16:22:45 -0500

    Learn state and create initial files for state folder, TheDate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import Split from './composition/Split';
+//import Split from './composition/Split';
 import './App.css';
-import Tooltip from './composition/Tooltip';
-import Messages from './Messages';
+//import Tooltip from './composition/Tooltip';
+//import Messages from './Messages';
+import TheDate from './state/TheDate';
 //testing intro example
 
 
@@ -11,43 +12,43 @@ class AppClass extends Component {
   render() {
     return (
       <div className="App">
-        <h1>YOUR APPLICATION NAME!</h1>
-        <div>Hello!</div>
+        <h1>YOUR APP NAME!</h1>
+        <TheDate />
         
-        <Messages name="Messages" unread={0}/>
-        <Messages name="Notifications" unread={10}/>
       </div>
     );
   }
 }
+//<Messages name="Messages" unread={0}/>
+//<Messages name="Notifications" unread={10}/>
 
 // make 2 tooltips here and another inside the App directly
-const firstTooltip = (
-  <Tooltip color="hotpink" message='tooltip message'>
-   ipsum {/*these are children*/}
-  </Tooltip>
-)
-const secondTooltip = (
-  <Tooltip color="#126BCC" message='another tooltip message'>
-    officiis {/*these are children*/}
-  </Tooltip>
-)
+//const firstTooltip = (
+//  <Tooltip color="hotpink" message='tooltip message'>
+//   ipsum {/*these are children*/}
+//  </Tooltip>
+//)
+//const secondTooltip = (
+//  <Tooltip color="#126BCC" message='another tooltip message'>
+//    officiis {/*these are children*/}
+//  </Tooltip>
+//)
 
-function App() {
-  return ( //2 instances of <Split />
-    <main className='App'>
-      <Split className='left' flexBasis={3}>
-  This is the content for the left `Split`. Star, {firstTooltip} Sun, Moon, Earth, Ocean, Sky. 
-        {/* make another tooltip directly inside the App */}
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus? {/*these are children*/}
-        </Tooltip>
-      </Split>
-      <Split className='right' flexBasis={4}>
-  This is the content for the right `Split`. Dragon Prince Zym {secondTooltip} is awesome.
-      </Split>
-    </main>
-  );
-}
+//function App() {
+//  return ( //2 instances of <Split />
+//    <main className='App'>
+//      <Split className='left' flexBasis={3}>
+//  This is the content for the left `Split`. Star, {firstTooltip} Sun, Moon, Earth, Ocean, Sky. 
+//        {/* make another tooltip directly inside the App */}
+//        <Tooltip message='one more tooltip message'>
+//          Necessitatibus? {/*these are children*/}
+//        </Tooltip>
+//      </Split>
+//      <Split className='right' flexBasis={4}>
+//  This is the content for the right `Split`. Dragon Prince Zym {secondTooltip} is awesome.
+//      </Split>
+//    </main>
+//  );
+//}
 
 export default AppClass;
